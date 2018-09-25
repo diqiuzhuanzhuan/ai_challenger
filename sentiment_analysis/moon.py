02d6381286ee22867c38a234a6b6914f06f690da
za-malong <malong@zhongan.io>
2018-09-25 11:15:30 +0800

    重新整理了数据的读取部分，生成器部分产生了将来要使用的one-hot编码的内容。

--- sentiment_analysis/moon.py
+++ sentiment_analysis/moon.py
@@ -7,6 +7,7 @@ email: diqiuzhuanzhuan@gmail.com
 """
 
 import tensorflow as tf
+from tensorflow.contrib import *
 from tensorflow.contrib import data
 from tensorflow.contrib import lookup
 import numpy as np
@@ -78,8 +79,12 @@ class LookMan(object):
     def __init__(self, dict_file, num_oov_buckets=1):
         self._num_oov = num_oov_buckets
         with open(dict_file, "r") as f:
-            word = f.readline().strip("\n")
-            self._box.add(word)
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                line = line.strip("\n")
+                self._box.add(line)
 
         k = 0
         for i in self._box:
@@ -89,11 +94,13 @@ class LookMan(object):
             k = k + 1
 
     def lookup(self, key):
-        try:
-            id = self._table[key]
-        except KeyError as e:
-            id = self._num_oov
-        return id
+        if key not in self._table:
+            return self._num_oov
+        return self._table[key]
+
+    def size(self):
+        return self._table.__len__() + 1
+
 
 
 class MoonLight(object):
@@ -103,38 +110,102 @@ class MoonLight(object):
 
     _batch_size = 32
     _table = None
+    _vocab_size = 0
 
     _train_iterator = None
     _train_batch = None
 
     _embedding_dimension = 50
+    _lstm_unit = 256
+    _lstm_layers = 2
+    _keep_prob = None
+    _attention_length = 5
 
     def __init__(self, embedding_dimension=100):
         self._embedding_dimension = embedding_dimension
+        self._keep_prob = tf.placeholder(dtype=tf.float32, name="keep_prob")
 
     def load(self):
         build_vocab()
         self._table = LookMan(DataFiles._dict_file, num_oov_buckets=1)
+        self._vocab_size = self._table.size()
+        print("字典文件加载完成，字典大小为: {}".format(self._vocab_size))
 
-    def _gen_train_data(self):
-        filenames = self._train_file_names
-        for file in filenames:
+    def _get_has_label_data(self, file_names):
+        for file in file_names:
             lines = pd.read_csv(file, delimiter=",", skiprows=1)
             for i in range(len(lines)):
                 sentence = lines.iloc[i, 1].strip("\"")
                 ids = [self._table.lookup(i) for i in sentence]
-                yield ids, lines.iloc[i, range(2, 22, 1)]
+                labels = []
+                for j in range(2, 22, 1):
+                    code = lines.iloc[1, j]
+                    # 标注数据可能取值为-2, -1, 0, 1, 为了onehot编码，归一化为0， 1， 2， 3
+                    code = code + 2
+                    labels.append(code)
+                #print(labels)
+                yield ids, [len(ids)], labels
+
+    def _get_no_label_data(self, file_names):
+        for file in file_names:
+            lines = pd.read_csv(file, delimiter=",", skiprows=1)
+            for i in range(len(lines)):
+                sentence = lines.iloc[i, 1].strip("\"")
+                ids = [self._table.lookup(i) for i in sentence]
+                yield ids, [len(ids)]
+
+    def _get_validation_data(self):
+        file_names = self._validation_file_names
+        yield from self._get_has_label_data(file_names)
+
+    def _gen_train_data(self):
+        file_names = self._train_file_names
+        yield from self._get_has_label_data(file_names)
+
+    def _get_test_data(self):
+        file_names = self._test_file_names
+        yield from self._get_no_label_data(file_names)
 
     def _load_train_data(self):
-        train_dataset = tf.data.Dataset.from_generator(self._gen_train_data, (tf.int64, tf.int64), ([None], [20]))
-        train_dataset = train_dataset.padded_batch(self._batch_size, padded_shapes=([None], [None]),
-                                                   padding_values=(tf.constant(-1, dtype=tf.int64), tf.constant(0, dtype=tf.int64)))
+        train_dataset = tf.data.Dataset.from_generator(self._gen_train_data, (tf.int64, tf.int64, tf.int64), ([None], [None], [20]))
+        train_dataset = train_dataset.padded_batch(self._batch_size, padded_shapes=([None], [None], [20]),
+                                                   padding_values=(tf.constant(1, dtype=tf.int64), tf.constant(0, dtype=tf.int64), tf.constant(0, dtype=tf.int64)))
+        train_dataset = train_dataset.map(lambda *x: (x[0], x[1], tf.one_hot(indices=x[2], depth=4, dtype=tf.int64)))
         self._train_iterator = train_dataset.make_initializable_iterator()
-        self._train_batch = self._train_iterator.get_next()
+        self._train_feature, self._train_feature_len, self._train_labels = self._train_iterator.get_next()
 
     def _create_embedding(self):
-        with tf.name_scope("create——embedding"):
-            self._embedding = tf.Variable()
+        with tf.name_scope("create_embedding"):
+            self._embedding_matrix = tf.get_variable(name="embedding_matrix", shape=[self._vocab_size, self._embedding_dimension],
+                                              initializer=tf.variance_scaling_initializer)
+            self._embedding = tf.nn.embedding_lookup(self._embedding_matrix, self._train_batch[0], name="embedding")
+
+    def _create_bilstm(self):
+
+        def lstm_cell(lstm_unit):
+            cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=lstm_unit, activation=tf.nn.relu)
+            cell = rnn.AttentionCellWrapper(cell=cell, attn_length=self._attention_length)
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell=cell, input_keep_prob=self._keep_prob)
+            return cell
+
+        with tf.name_scope("create_bilstm"):
+            # lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=self._lstm_unit, activation=tf.nn.relu)
+            stack_fw_lstm = tf.nn.rnn_cell.MultiRNNCell(
+                [lstm_cell(lstm_unit=self._lstm_unit) for _ in range(self._lstm_layers)]
+            )
+            initial_state_fw = stack_fw_lstm.zero_state(self._batch_size, tf.float32)
+            stack_bw_lstm = tf.nn.rnn_cell.MultiRNNCell(
+                [lstm_cell(lstm_unit=self._lstm_unit) for _ in range(self._lstm_layers)]
+            )
+            initial_state_bw = stack_bw_lstm.zero_state(self._batch_size, tf.float32)
+            self._sentence_encoder_output, self._sentence_encoder_state = \
+                rnn.stack_bidirectional_dynamic_rnn(cells_fw=stack_fw_lstm, cells_bw=stack_bw_lstm, initial_states_fw=initial_state_fw,
+                                                    initial_states_bw=initial_state_bw, inputs=self._embedding)
+
+    def _create_loss(self):
+        with tf.name_scope("create_loss"):
+            pass
+            #self._loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=self._sentence_encoder_state, labels=)
 
 
 if __name__ == "__main__":
@@ -147,6 +218,6 @@ if __name__ == "__main__":
         sess.run(tf.tables_initializer())
         while True:
             try:
-                print(sess.run(ml._train_batch))
+                print(sess.run([ml._train_feature, ml._train_feature_len, ml._train_labels]))
             except tf.errors.OutOfRangeError:
                 break
