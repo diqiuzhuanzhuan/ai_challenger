e89f171f7a1b6edff3c7a16385e36c3626ed8bcf
diqiuzhuanzhuan <malong@zhongan.io>
2018-10-11 11:39:51 +0800

    1. 默认使用词向量。2. cnn代码编写部分embedding内容。

--- sentiment_analysis/text_cnn.py
+++ sentiment_analysis/text_cnn.py
@@ -8,6 +8,7 @@ email: diqiuzhuanzhuan@gmail.com
 
 import tensorflow as tf
 import numpy as np
+from sea import DataFiles, Data
 
 
 class TextCNN(object):
@@ -15,25 +16,22 @@ class TextCNN(object):
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
-    def __init__(
-      self, sequence_length, num_classes, vocab_size,
-      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
 
+    def __init__(
+            self, sequence_length, num_classes, vocab_size,
+            embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+
+        self._embedding_size = embedding_size
+        self._vocab_size = vocab_size
+        self._batch_size = tf.placeholder(dtype=tf.int64, name="batch_size")
         # Placeholders for input, output and dropout
-        self.feature = tf.placeholder(tf.int32, [None, sequence_length], name="feature")
-        self.label = tf.placeholder(tf.float32, [None, num_classes], name="label")
+        self._feature = tf.placeholder(tf.int32, [None, sequence_length], name="feature")
+        self._label = tf.placeholder(tf.float32, [None, num_classes], name="label")
         self._keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+        self._data = Data(self._batch_size)
 
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
-
-        # Embedding layer
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            self.W = tf.Variable(
-                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.feature)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
@@ -67,7 +65,7 @@ class TextCNN(object):
 
         # Add dropout
         with tf.name_scope("dropout"):
-            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self._keep_prob)
 
         # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
@@ -83,10 +81,27 @@ class TextCNN(object):
 
         # Calculate mean cross-entropy loss
         with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.label)
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self._label)
             self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
         # Accuracy
         with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(self.predictions, tf.argmax(self.label, 1))
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self._label, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
+    def _load_data(self):
+        self._train_iterator, self._train_iterator_initializer, self._validation_iterator, self._validation_iterator_initializer, self._test_iterator, self._test_iterator_initializer \
+            = self._data.load_data()
+        self._validation_next = self._validation_iterator.get_next()
+        self._actual_batch_size = tf.shape(self._feature)[0]
+        self.weights = tf.constant(self._data.weights)
+
+    def _create_embedding(self):
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(
+                tf.random_uniform([self._vocab_size, self._embedding_size], -1.0, 1.0),
+                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self._feature)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
